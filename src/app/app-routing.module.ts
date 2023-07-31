0f440fa6cc898cbb162268f6d9d76c546f635fa8
mycreate2001 <mycreate2001@gmail.com>
2023-07-31 21:38:59 +0700

    slide menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,8 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'menu',
+    redirectTo: 'tim-chu',
     pathMatch: 'full'
-  },
-  {
-    path: 'menu',
-    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
   },
   {
     path: 'tim-chu',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  pages=[
+    {name:'Tìm chữ',image:'abc.png',note:'giúp bé làm quen với bảng chữ cái',url:'tim-chu'},
+    {name:'Đoán từ',image:'abc.png',note:'Giúp bé đoán chữ cái',url:'doan-tu'}
+  ]
   constructor() {}
 }
